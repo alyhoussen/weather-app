ab3152ead4416d1a35ca09a5f7dd667065734c50
dhaxdev <oussenalyjoma@gmail.com>
2024-12-02 22:17:00 +0100

    Modified logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="xs:flex justify-center fixed w-full t-[0] header p-[1rem] z-[100]">
         <div className="flex justify-between sm:w-[700px] items-center xs:w-[350px]">
-            <a href=""><h1 className="text-[yellow] flex gap-2 items-center text-[22px]"> <FaCloudSun className="" />IjeryMetéo</h1></a>
+            <a href=""><h1 className="text-[yellow] flex gap-2 items-center text-[22px]"> <FaCloudSun className="" />IjeryMétéo</h1></a>
             <a href="https://github.com/dhaxdev/weather-app" className="text-[#fff]"><BsGithub className="text-[25px]"/></a>
         </div>
     </div>
