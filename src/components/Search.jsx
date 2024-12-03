0b6195cdf178e4f2970525670b4ae589787d7063
dhaxdev <oussenalyjoma@gmail.com>
2024-12-03 12:02:22 +0100

    Another litle modification

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,16 +8,16 @@ function Search({styles, getMeteos, setLocation, setPosition}) {
   const [loading, setLoading] = useState(false)
   const abortControllerRef = useRef(null)
   const getSuggestions = async ()=>{
-    if (abortControllerRef.current){
-      abortControllerRef.current.abort()
-    }
+    // if (abortControllerRef.current){
+    //   abortControllerRef.current.abort()
+    // }
 
     const controller = new AbortController()
     abortControllerRef.current = controller
 
     try{
       setLoading(true)
-      const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${input}&format=json&addressdetails=1&limit=20`,{signal: controller.signal})
+      const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${input}&format=json&type=city&addressdetails=1&limit=20`,{signal: controller.signal})
       const data = await response.json();
       setSuggestions(data) 
       return 1
@@ -29,7 +29,7 @@ function Search({styles, getMeteos, setLocation, setPosition}) {
   }
   const handleOnChange = (value)=>{
     setInput(value);
-    getSuggestions()
+    // getSuggestions()
     
   }
   const handleSuggestionClick = async (lat, lng, ville)=>{
@@ -41,15 +41,15 @@ function Search({styles, getMeteos, setLocation, setPosition}) {
   // const
   return (
     <div className="mb-3">
-      <form onSubmit={(e)=>{e.preventDefault()}} className={` ${styles} p-[1rem] py-[0.5rem] card rounded-[50px] min-w-[100%] flex-1 items-center xs:min-w-[350px] justify-between gap-2`}>
+      <form onSubmit={(e)=>{e.preventDefault();getSuggestions()}} className={` ${styles} p-[1rem] py-[0.5rem] card rounded-[50px] min-w-[100%] flex-1 items-center xs:min-w-[350px] justify-between gap-2`}>
           <label htmlFor="search" className="cursor-pointer"><FaSearch /></label>
           <input type="search" id="search" value={input} onChange={(e)=>{handleOnChange(e.target.value)}} className="flex-1 outline-none bg-transparent" placeholder="Search a City"/>
       </form>
       {input &&
         <h2 className="bg-white  mx-4 mt-1 t-2 text-primary p-2 px-4 rounded-[5px]">  
         {suggestions.length == 0 ?
-          "No suggestions"
-          : (suggestions && <> {loading ? <div className="flex justify-center p-[1.5rem]"><div className="spinner"></div></div>:suggestions.map((suggestion,index)=>( <p key={index} className="cursor-pointer" onClick={()=> {handleSuggestionClick(suggestion.lat,suggestion.lon,`${suggestion.adrres.state} ${suggestion.address.city} ${suggestion.address.country}`)}}>{suggestion.adrres.state} {suggestion.address.city} {suggestion.address.country}</p> ))}</>)
+          "No result"
+          : (suggestions && <> {loading ? <div className="flex justify-center p-[1.5rem]"><div className="spinner"></div></div>:suggestions.map((suggestion,index)=>( <p key={index} className="cursor-pointer" onClick={()=> {handleSuggestionClick(suggestion.lat,suggestion.lon,`${suggestion.address.state} ${suggestion.address.city} ${suggestion.address.country}`)}}>{suggestion.address.state} {suggestion.address.city} {suggestion.address.country}</p> ))}</>)
         }
         </h2>
       }
