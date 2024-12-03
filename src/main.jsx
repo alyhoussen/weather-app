d811805b0e0cca8cab6662b6f8b28fcdc5d78948
dhaxdev <oussenalyjoma@gmail.com>
2024-12-03 06:57:46 +0100

    Fixed Footer import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Footer from './components/footer.jsx'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <div className='min-h-full root absolute w-full flex flex-col'>
