0a346799c3578adeacd951596e0391d443d0c7c7
Houssen D. Aly <159697794+alyhoussen@users.noreply.github.com>
2025-03-20 16:41:44 +0300

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -42,8 +42,7 @@ function Search({styles, getMeteos, setLocation, setPosition}) {
   return (
     <div className="mb-3">
       <form onSubmit={(e)=>{e.preventDefault();getSuggestions()}} className={` ${styles} p-[1rem] py-[0.5rem] card rounded-[50px] min-w-[100%] flex-1 items-center xs:min-w-[350px] justify-between gap-2`}>
-          <label htmlFor="search" className="cursor-pointer"><FaSearch />Ama</label>
-          <input type="search" id="search" value={input} onChange={(e)=>{handleOnChange(e.target.value)}} className="flex-1 outline-none bg-transparent" placeholder="Search a City"/>
+        <input type="search" id="search" value={input} onChange={(e)=>{handleOnChange(e.target.value)}} className="flex-1 outline-none bg-transparent" placeholder="Search a City"/>
       </form>
       {input &&
         <h2 className="bg-white  mx-4 mt-1 t-2 text-primary p-2 px-4 rounded-[5px]">  
